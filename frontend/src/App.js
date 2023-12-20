9dd7bdc7e1a14ee90b61e983f7fdf56e9cc25f51
eshwarnamala <namalaeshwar0@gmail.com>
2023-12-20 22:32:15 +0530

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   const searchProfile = async (username) => {
     try {
-      const response = await axios.get(`http://localhost:5000/github-profiles/${username}`);
+      const response = await axios.get(`http://localhost:3000/github-profiles/${username}`);
       setUserData(response.data);
     } catch (error) {
       console.error('Error fetching user data:', error);
